6a0a4b51e5b36bf02fcc90bba1bbb13ca561d62c
Syspretor <32930733+Syspretor@users.noreply.github.com>
2018-11-18 23:45:32 +0800

    Update run_executable_files.py

--- run_executable_files.py
+++ run_executable_files.py
@@ -11,12 +11,12 @@ import subprocess
 
 #used to judge if a file is a executable file by "ll -x command"
 def judge_executable(dirname):
-	judge_executable=os.system("ll -x "+file_path+" | grep -q \"*\"")
-	#&&("file"+file_path+" | grep -q \"executable\"")
-	if judge_executable==True:
-		return True
-	else:
-		return False
+    judge_executable=os.system("ll -x "+dirname+" | grep -q \"*\"")
+    #&&("file"+file_path+" | grep -q \"executable\"")
+    if judge_executable==True:
+	return True
+    else:
+	return False
 
 #Used to traversing all the executable files in specified directory and sort these in lexicographic order
 #return a list contains all the executable file path
@@ -34,17 +34,16 @@ def traversing_dir(dirname):
             file_path = os.path.join(maindir, filename)#Merge into one full path
             #file_type= os.path.splitext(apath)[0]  #Get the file suffix [0] to get something other than the file name
 
-			if judge_executable(file_path)==True:
-				result.append(file_path)
+	    if judge_executable(file_path)==True:
+	        result.append(file_path)
 
     return sorted(result)#return a list contains all the executable file path which sorted in lexicograhic orderr
 
 #main module
 dirname=input('please input the directory:')
 sorted_result=traversing_dir(dirname)
-i=0
 for i in range(len(sorted_result)):
-	try:
-		subprocess.call(sorted_result[i])
-	except OSError as e:
-		print("Execution failed when running",sorted_result[i],":",e)
+    try:
+	subprocess.call(sorted_result[i])
+    except OSError as e:
+	print("Execution failed when running",sorted_result[i],":",e)
